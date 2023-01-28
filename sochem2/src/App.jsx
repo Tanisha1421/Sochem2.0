8e132e99561980cce65c51d10102819c334f4d9f
Devansh <choudhary.devansh1007@gmail.com>
2023-01-28 10:04:44 +0530

    added routing for events page

--- sochem2/src/App.jsx
+++ sochem2/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Family from "./pages/Family/Family";
 import Home from "./pages/Home/Home";
 import Navbar from "./common/Navbar/Navbar";
+import Events from "./pages/Events/Events";
 
 export default function App() {
   return (
@@ -12,9 +13,10 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/family" element={<Family />} />
+            <Route path="/events" element={<Events />} />
           </Routes>
         </div>
-      </div>      
+      </div>
     </Router>
   );
 }
---
+++ sochem2/src/pages/Events/Events.jsx
@@ -0,0 +1,9 @@
+function Events() {
+  return (
+    <div id="events-container">
+      <h2> This is the Events page :) </h2>
+    </div>
+  );
+}
+
+export default Events;
